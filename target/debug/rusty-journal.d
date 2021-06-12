66900be2d90d75cb906833f1284d75d0eaac8740
Eriberto Mendz <eribert.mendez@gmail.com>
2021-06-12 19:26:38 +0300

    add all commits

--- target/debug/rusty-journal.d
+++ target/debug/rusty-journal.d
@@ -1,1 +1,1 @@
-/home/eribertt/coding/rust/rusty-journal/target/debug/rusty-journal: /home/eribertt/coding/rust/rusty-journal/src/cli.rs /home/eribertt/coding/rust/rusty-journal/src/main.rs
+/home/eribertt/rust_projects/rusty-journal/target/debug/rusty-journal: /home/eribertt/rust_projects/rusty-journal/src/cli.rs /home/eribertt/rust_projects/rusty-journal/src/main.rs
